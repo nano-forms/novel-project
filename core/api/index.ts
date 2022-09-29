07af34104656417d8977a76f0aacc37eef403d41
Barend Erasmus <hirebarend@gmail.com>
2022-09-29 14:17:54 +0200

    update

--- core/api/index.ts
+++ core/api/index.ts
@@ -1,3 +1,7 @@
 import { FormClient } from "@nano-forms/core";
 
-export const formClient: FormClient = new FormClient(null);
+export const formClient: FormClient = new FormClient(
+  null,
+  "5011780f-65b5-4840-8a32-82690afdb9a7",
+  "401637c7-5e1e-492f-bf2b-e82dcb4e5cad"
+);
